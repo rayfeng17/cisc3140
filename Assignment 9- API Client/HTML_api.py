2c008b0cec77e84fcccda8fa835fe6b87dfd4b5d
Raymond Feng <54931410+rayfeng17@users.noreply.github.com>
2020-04-12 15:38:21 -0400

    Update HTML_api.py

--- Assignment 9- API Client/HTML_api.py
+++ Assignment 9- API Client/HTML_api.py
@@ -9,6 +9,7 @@ querystring = {"c": cat_drink["strCategory"]}
 headers = {
     'x-rapidapi-host': "the-cocktail-db.p.rapidapi.com",
     'x-rapidapi-key': "Your API key"
+    # "5dc11f6bd4msh47eeca06b2dae52p1b3ef2jsn7da9857fe2f5"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
