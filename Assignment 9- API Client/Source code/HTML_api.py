9f56769dd3a198665d98099dcdf3b4fd39fad51c
Raymond Feng <54931410+rayfeng17@users.noreply.github.com>
2020-04-16 13:36:33 -0400

    Update HTML_api.py

--- Assignment 9- API Client/Source code/HTML_api.py
+++ Assignment 9- API Client/Source code/HTML_api.py
@@ -1,6 +1,29 @@
 import json
 import requests
-from main_api import cat_drink
+
+url = "https://the-cocktail-db.p.rapidapi.com/list.php"
+
+querystring = {"c": "list"}
+
+headers = {
+    'x-rapidapi-host': "the-cocktail-db.p.rapidapi.com",
+    'x-rapidapi-key': "5dc11f6bd4msh47eeca06b2dae52p1b3ef2jsn7da9857fe2f5"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+category_list = json.loads(response.text)
+
+print(" Available Cocktail Category : ")
+i = 0
+for item in (category_list["drinks"]):
+    print("  " + str(i) + " " + item['strCategory'])
+    i = i + 1
+
+ip_category = input("Enter Category # : ")
+
+print("Category chosen : ", category_list["drinks"][int(ip_category)])
+cat_drink = category_list["drinks"][int(ip_category)]
 
 url = "https://the-cocktail-db.p.rapidapi.com/filter.php"
 
@@ -8,8 +31,7 @@ querystring = {"c": cat_drink["strCategory"]}
 
 headers = {
     'x-rapidapi-host': "the-cocktail-db.p.rapidapi.com",
-    'x-rapidapi-key': "Your API key"
-    # "5dc11f6bd4msh47eeca06b2dae52p1b3ef2jsn7da9857fe2f5"
+    'x-rapidapi-key': "5dc11f6bd4msh47eeca06b2dae52p1b3ef2jsn7da9857fe2f5"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
